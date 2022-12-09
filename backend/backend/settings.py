6ced35bd898fb8b54eb72fb6899f0d8053786f26
Искров Антон <iskrov.95@mail.ru>
2022-12-09 11:37:16 +0300

    after 1 review

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -4,10 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
-SECRET_KEY = os.getenv('SECRET_KEY', default='c!fdcy^l2zux3e+50zx$5$-xl6)$=yrgt_3-mkx-_6fy@fkh#%')
+SECRET_KEY = os.getenv('SECRET_KEY', default='secret_key')
 
 DEBUG = True
 
